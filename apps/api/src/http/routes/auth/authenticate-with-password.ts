aba85a3254c965566843b30f0d8727520c9a74a5
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-05-23 09:27:20 -0300

    feat: create error handling

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -5,6 +5,8 @@ import z from 'zod'
 
 import { prisma } from '@/lib/prisma'
 
+import { BadRequestError } from '../_errors/bad-request-error'
+
 export async function authenticateWithPassword(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/sessions/password',
@@ -18,12 +20,6 @@ export async function authenticateWithPassword(app: FastifyInstance) {
           password: z.string().min(6),
         }),
         response: {
-          400: z.object({
-            message: z.string(),
-          }),
-          401: z.object({
-            message: z.string(),
-          }),
           201: z.object({
             token: z.string(),
           }),
@@ -38,13 +34,13 @@ export async function authenticateWithPassword(app: FastifyInstance) {
       })
 
       if (!userFromEmail) {
-        return reply.status(400).send({ message: 'Invalid email or password' })
+        throw new BadRequestError('Invalid email or password')
       }
 
       if (userFromEmail.passwordHash === null) {
-        return reply
-          .status(400)
-          .send({ message: 'User does not have a password, use social login' })
+        throw new BadRequestError(
+          'User does not have a password, use social login',
+        )
       }
 
       const isPasswordValid = await compare(
@@ -53,7 +49,7 @@ export async function authenticateWithPassword(app: FastifyInstance) {
       )
 
       if (!isPasswordValid) {
-        return reply.status(400).send({ message: 'Invalid email or password' })
+        throw new BadRequestError('Invalid email or password')
       }
 
       const token = await reply.jwtSign(
