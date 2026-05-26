b6bcfd66ad01925f671301fed451ec086d0fd6e0
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-05-26 20:17:30 -0300

    feat: created route for get membership in a organization

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,47 @@
+import { rolesSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organization/:slug/membership',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Get user membership on organization',
+          description: 'Get user membership on organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.uuid(),
+                role: rolesSchema,
+                organizationId: z.uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+        const { membership } = await request.getUserMembership(slug)
+
+        return {
+          membership: {
+            id: membership.id,
+            role: rolesSchema.parse(membership.role),
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
+}
