81c9f28b3dc654ea8bbdf4585f41e421713d2916
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-05-27 19:19:06 -0300

    feat: create route to list organization details and organizations, where the current user is a member

---
+++ apps/api/src/http/routes/orgs/get-organizations.ts
@@ -0,0 +1,76 @@
+import { rolesSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+export async function getOrganizations(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Get organizations where user is a member',
+          description:
+            'Get details from organizations where the user is a member.',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              organizations: z.array(
+                z.object({
+                  id: z.uuid(),
+                  name: z.string(),
+                  slug: z.string(),
+                  avatarUrl: z.url().nullable(),
+                  role: rolesSchema,
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const userId = await request.getCurrentUserId()
+
+        const organizations = await prisma.organization.findMany({
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+            avatarUrl: true,
+            members: {
+              select: {
+                role: true,
+              },
+              where: {
+                userId,
+              },
+            },
+          },
+          where: {
+            members: {
+              some: {
+                userId,
+              },
+            },
+          },
+        })
+
+        const organizationsWithRole = organizations.map(
+          ({ members, ...org }) => {
+            return {
+              ...org,
+              role: members[0].role,
+            }
+          },
+        )
+
+        return { organizations: organizationsWithRole }
+      },
+    )
+}
