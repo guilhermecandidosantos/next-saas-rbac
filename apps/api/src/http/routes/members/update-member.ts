ad4131be6f135beb6e9ece49147efbdd12abb481
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-06-04 20:38:02 -0300

    feat: create a route to update a member in the same organization

---
+++ apps/api/src/http/routes/members/update-member.ts
@@ -0,0 +1,65 @@
+import { rolesSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-persmissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function updateMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Update a member',
+          description: 'Update a member within the specified organization.',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string(),
+          }),
+          body: z.object({
+            role: rolesSchema,
+          }),
+          response: {
+            204: z.object({}),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError(
+            "You're not allowed to update members in this organization.",
+          )
+        }
+
+        const { role } = request.body
+
+        await prisma.member.update({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+          data: {
+            role,
+          },
+        })
+
+        return reply.status(204).send({})
+      },
+    )
+}
