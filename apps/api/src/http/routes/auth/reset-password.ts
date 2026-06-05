45a842865975312529467b983ea86af1747cad32
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-06-04 21:58:20 -0300

    feat: delete the password reset token after use

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -39,10 +39,17 @@ export async function resetPassword(app: FastifyInstance) {
 
       const passowordHash = await hash(password, 6)
 
-      await prisma.user.update({
-        where: { id: tokenFromCode.userId },
-        data: { passwordHash: passowordHash },
-      })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: { id: tokenFromCode.userId },
+          data: { passwordHash: passowordHash },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ])
 
       return reply.status(204).send({})
     },
