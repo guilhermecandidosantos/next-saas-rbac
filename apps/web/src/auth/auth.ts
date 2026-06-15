db6964517b0f9550e053dcccfd535c627d366d1e
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-06-15 20:30:19 -0300

    feat: create route to retrieve user profile

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,8 +1,31 @@
 'use server'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookiesStore = await cookies()
 
   return cookiesStore.get('token')?.value
+}
+
+export async function auth() {
+  const cookiesStore = await cookies()
+
+  const token = cookiesStore.get('token')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch (error) {
+    cookiesStore.delete('token')
+  }
+
+  redirect('/auth/sign-in')
 }
--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,25 @@
+import { type CookiesFn, getCookie } from 'cookies-next'
 import ky from 'ky'
 
 export const api = ky.create({
   prefix: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async ({ request }) => {
+        let cookieStore: CookiesFn | undefined
+
+        if (typeof window === 'undefined') {
+          const { cookies } = await import('next/headers')
+
+          cookieStore = cookies
+        }
+
+        const token = await getCookie('token', { cookies: cookieStore })
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
