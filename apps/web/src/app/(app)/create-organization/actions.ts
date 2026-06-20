0160954008763c0a64bd5185e1e9e668f444a592
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-06-20 09:41:56 -0300

    feat:create page to create a new organization

---
+++ apps/web/src/app/(app)/create-organization/actions.ts
@@ -0,0 +1,92 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+import { createOrganization } from '@/http/create-organization'
+
+const organizationSchema = z
+  .object({
+    name: z.string().min(4, {
+      message: 'Organization name must be at least 4 characters long',
+    }),
+    domain: z
+      .string()
+      .nullable()
+      .refine(
+        (value) => {
+          if (value) {
+            const domainRegex = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+            return domainRegex.test(value)
+          }
+
+          return true
+        },
+        {
+          message: 'Invalid domain format',
+        },
+      ),
+    shouldAttachUsersByDomain: z
+      .union([z.literal('on'), z.literal('off'), z.boolean()])
+      .transform((value) => value === 'on' || value === true)
+      .default(false),
+  })
+  .refine(
+    (data) => {
+      if (data.shouldAttachUsersByDomain === true && !data.domain) {
+        return false
+      }
+      return true
+    },
+    {
+      message: 'Domain is required when auto-join by domain is enabled',
+      path: ['domain'],
+    },
+  )
+
+export async function createOrganizationAction(data: FormData) {
+  const result = organizationSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = z.flattenError(result.error).fieldErrors
+    return {
+      success: false,
+      message: null,
+      errors,
+    }
+  }
+
+  const { name, domain, shouldAttachUsersByDomain } = result.data
+
+  try {
+    await createOrganization({
+      name: String(name),
+      domain: domain ? String(domain) : null,
+      shouldAttachUsersByDomain: Boolean(shouldAttachUsersByDomain),
+    })
+
+    return {
+      success: true,
+      message: 'Successfully saved organization.',
+      errors: null,
+    }
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = err.data
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'An unexpected error occurred. Please, try again later.',
+      errors: null,
+    }
+  }
+}
