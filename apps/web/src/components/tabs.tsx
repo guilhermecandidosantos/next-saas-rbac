e2a1926281d0b8265ac0c2701815b56f839709b3
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-06-23 20:03:58 -0300

    feat: create tab navigation component

---
+++ apps/web/src/components/tabs.tsx
@@ -0,0 +1,56 @@
+import { ability, getCurrentOrg } from '@/auth/auth'
+
+import { NavLink } from './nav-link'
+import { Button } from './ui/button'
+
+export async function Tabs() {
+  const currentOrg = await getCurrentOrg()
+
+  const permissions = await ability()
+
+  const canUpdateOrganization = permissions?.can('update', 'Organization')
+  const canGetBilling = permissions?.can('get', 'Billing')
+  const canGetMembers = permissions?.can('get', 'User')
+  const canGetProjects = permissions?.can('get', 'Project')
+
+  return (
+    <div className="border-b py-4">
+      <nav className="mx-auto flex max-w-[1200px] items-center gap-2">
+        {canGetProjects && (
+          <Button
+            asChild
+            className="text-muted-foreground data-[current=true]:text-foreground data-[current=true]:border-border cursor-pointer border border-transparent"
+            size="sm"
+            variant="ghost"
+          >
+            <NavLink href={`/org/${currentOrg}`}>Projects</NavLink>
+          </Button>
+        )}
+
+        {canGetMembers && (
+          <Button
+            asChild
+            className="text-muted-foreground data-[current=true]:text-foreground data-[current=true]:border-border cursor-pointer border border-transparent"
+            size="sm"
+            variant="ghost"
+          >
+            <NavLink href={`/org/${currentOrg}/members`}>Members</NavLink>
+          </Button>
+        )}
+
+        {(canUpdateOrganization || canGetBilling) && (
+          <Button
+            asChild
+            className="text-muted-foreground data-[current=true]:text-foreground data-[current=true]:border-border cursor-pointer border border-transparent"
+            size="sm"
+            variant="ghost"
+          >
+            <NavLink href={`/org/${currentOrg}/settings`}>
+              Settings & Billing
+            </NavLink>
+          </Button>
+        )}
+      </nav>
+    </div>
+  )
+}
