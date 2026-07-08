dba5ede4711aa9fd761cd11d20cf786647e2928f
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-07-08 00:46:35 -0300

    style: adjust header component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -15,7 +15,7 @@ export async function Header() {
   const permissions = await ability()
 
   return (
-    <div className="2 mx-auto flex max-w-[1200px] items-center justify-between">
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image
           src={rocketseatIcon}
