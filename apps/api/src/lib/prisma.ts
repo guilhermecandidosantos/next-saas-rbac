c31cb22baa4d17d015b7a325d43ba2eef02b55b3
Guilherme Santos <guilhermecandidosantos1998@gmail.com>
2026-07-08 10:06:53 -0300

    fix: adjusted import PrismaClient

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 
 import { PrismaPg } from '@prisma/adapter-pg'
 
-import { PrismaClient } from '@/prisma/client/prisma'
+import { PrismaClient } from '../generated/prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
